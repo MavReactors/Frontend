eb5f4a858feb704ec8b6306909d32c673914502f
Miguel Mahecha <118125049+MiguelMahecha@users.noreply.github.com>
2024-05-15 18:22:42 -0500

    feat: added SignUpPage, as well as react-router-dom for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 // import './App.css'
 import {LogInPage} from "@/page/LogInPage.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {SignUpPage} from "@/page/SignUpPage.tsx";
 
 function App() {
 
   return (
     <>
-        <LogInPage></LogInPage>
+        <BrowserRouter>
+            <Routes>
+                <Route path={'/'} element={<LogInPage />} ></Route>
+                <Route path={'/signup'} element={<SignUpPage />}> </Route>
+            </Routes>
+        </BrowserRouter>
     </>
   )
 }
