163236ae4073be0f960488cdf9d188a3ef384deb
Miguel Mahecha <118125049+MiguelMahecha@users.noreply.github.com>
2024-05-16 20:49:19 -0500

    feat: Made progress on WardrobePage.

---
+++ src/models/clotheType.ts
@@ -0,0 +1,12 @@
+export enum ClotheType {
+    SHIRT = 'SHIRT',
+    PANTS = 'PANTS',
+    DRESS = 'DRESS',
+    SKIRT = 'SKIRT',
+    JACKET = 'JACKET',
+    COAT = 'COAT',
+    SNEAKERS = 'SNEAKERS',
+    BOOTS = 'BOOTS',
+    SANDALS = 'SANDALS',
+    HEELS = 'HEELS',
+}
--- src/page/WardrobePage.tsx
+++ src/page/WardrobePage.tsx
@@ -7,16 +7,53 @@ import {
     NavigationMenuTrigger,
 } from "@/components/ui/navigation-menu"
 import {Link} from "react-router-dom";
+import {Button} from "@/components/ui/button.tsx";
+import React from "react";
+
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardImage,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+
+
+import clotheItem from '@/data/clotheItem.json'
 
 export default function WardrobePage() {
+    const clotheTypes = [
+        'SHIRT',
+        'PANTS',
+        'DRESS',
+        'SKIRT',
+        'JACKET',
+        'COAT',
+        'SNEAKERS',
+        'BOOTS',
+        'SANDALS',
+        'HEELS']
+
+    console.table(clotheItem[0])
+
+    const [activeClotheType, setActiveClotheType] =
+        React.useState<string | undefined>(undefined);
+
+    const handleClick = (type: string) => {
+        console.log(type)
+        setActiveClotheType(type)
+    }
+
     return (
         <>
             <NavigationMenu>
                 <NavigationMenuList>
                     <NavigationMenuItem>
-                        <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
+                        <NavigationMenuTrigger>Navigate</NavigationMenuTrigger>
                         <NavigationMenuContent>
-                            <NavigationMenuLink>
+                            <NavigationMenuLink className='p-6'>
                                 <Link to={'/signup'}>Back</Link>
                             </NavigationMenuLink>
                         </NavigationMenuContent>
@@ -24,6 +61,35 @@ export default function WardrobePage() {
                 </NavigationMenuList>
             </NavigationMenu>
 
+            <div className="grid grid-cols-[1fr_auto] gap-4 h-[80vh] p-6">
+                <div className="showcase grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3">
+                    {clotheItem
+                        .filter(item => item.tipo === activeClotheType)
+                        .map((item, index) => (
+                        <Card key={index} className={'w-[250px] p-3'}>
+                            <CardImage src={item.foto} className=''></CardImage>
+                            <CardHeader>
+                                <CardTitle>{`${item.tipo}`}</CardTitle>
+                                <CardDescription>{`Último Uso: ${item.ultimoUso}`}</CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p>{`Último Lavado: ${item.ultimoLavado}`}</p>
+                            </CardContent>
+                            <CardFooter>
+                                <p>{`Se Plancha: ${item.sePlancha ? 'Sí' : 'No'}`}</p>
+                            </CardFooter>
+                        </Card>
+                    ))
+                    }
+                </div>
+                <div className="picker flex flex-col gap-2">
+                    {clotheTypes.map((type, index) => (
+                        <Button key={index} onClick={() => handleClick(type)}>
+                            {type}
+                        </Button>
+                    ))}
+                </div>
+            </div>
         </>
     )
 }
