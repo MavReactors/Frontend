464eaac6555cb77b8b0910bde5f1655100dc0a18
Joan-Acevedo <acevedojoan22@gmail.com>
2024-05-23 01:50:52 -0500

    add page without back

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,24 @@
-// import './App.css'
-import {LogInPage} from "@/page/LogInPage.tsx";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {SignUpPage} from "@/page/SignUpPage.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LogInPage } from "@/page/LogInPage.tsx";
+import { SignUpPage } from "@/page/SignUpPage.tsx";
 import WardrobePage from "@/page/WardrobePage.tsx";
-import {LandingPage} from "@/page/LandingPage.tsx";
+import { LandingPage } from "@/page/LandingPage.tsx";
+import { TrendsPage } from "@/page/TrendPage.tsx";
+import { SchedulePage } from "@/page/SchedulePage.tsx";
 
 function App() {
-
-  return (
-    <>
+    return (
         <BrowserRouter>
             <Routes>
-                <Route path={'/'} element={<LandingPage/>} ></Route>
-                <Route path={'/login'} element={<LogInPage />} ></Route>
-                <Route path={'/signup'} element={<SignUpPage />}> </Route>
-                <Route path={'/wardrobe'} element={<WardrobePage />}> </Route>
+                <Route path={'/'} element={<LandingPage />} />
+                <Route path={'/login'} element={<LogInPage />} />
+                <Route path={'/signup'} element={<SignUpPage />} />
+                <Route path={'/wardrobe'} element={<WardrobePage />} />
+                <Route path={'/trends'} element={<TrendsPage />} />
+                <Route path={'/schedule'} element={<SchedulePage />} />
             </Routes>
         </BrowserRouter>
-    </>
-  )
+    )
 }
 
 export default App
