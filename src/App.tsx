19fc45aa278b5c7d3224a73a0d148548ebe8628a
andreec2 <estrellaof198425@gmail.com>
2024-05-22 21:50:45 -0500

    configuracion de usuarios

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {SignUpPage} from "@/page/SignUpPage.tsx";
 import WardrobePage from "@/page/WardrobePage.tsx";
 import {LandingPage} from "@/page/LandingPage.tsx";
+import EditPorfile from "./page/EditPorfile";
 
 function App() {
 
@@ -15,6 +16,7 @@ function App() {
                 <Route path={'/login'} element={<LogInPage />} ></Route>
                 <Route path={'/signup'} element={<SignUpPage />}> </Route>
                 <Route path={'/wardrobe'} element={<WardrobePage />}> </Route>
+                <Route path={'/editPorfile'} element={<EditPorfile/>}> </Route>
             </Routes>
         </BrowserRouter>
     </>
